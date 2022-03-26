d858e26fc616586181ba0db44f1cab9a8bdc7a19
RobertoInz <roberto_inz@hotmail.com>
2022-03-26 16:26:43 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ComponenteBoco 
+      v-for="event in timeline" :key="event.title" 
+      :title="event.title" 
+      :content="event.content"
+    ></ComponenteBoco>
+  </div>
+</template>
+
+<script>
+
+import ComponenteBoco from "./components/ComponenteBoco";
+
+export default {
+  name: 'App',
+  components: {
+    ComponenteBoco
+  },
+  data() {
+    return {
+      timeline: [
+        {
+          title: "Início em 1980",
+          content: "Aqui começa a putaria"
+        },
+        {
+          title: "Início em 1981",
+          content: "Aqui continua a putaria"
+        },
+        {
+          title: "Início em 1982",
+          content: "Aqui morre a putaria"
+        },
+        {
+          title: "Início em 1983",
+          content: "Aqui volta a putaria"
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ComponenteBoco.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h2>{{title}}</h2>
+		<p>{{content}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			title: String,
+			content: String,
+		}
+	}
+</script>
