27da720a0e34e59c9bead6715d16bd739b1413a1
RobertoInz <roberto_inz@hotmail.com>
2022-04-02 14:04:53 -0300

    Criando base da home #1

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,25 @@
       :title="event.title" 
       :content="event.content"
     ></ComponenteBoco>
+        <nav> 
+         <li><a href="">Home</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">Profile</a></li>
+            <li><a href="">Biography</a></li>
+            <li><a href="">Discography</a></li>
+            <li><a href="">Schedule</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Link</a></li>
+        </nav> 
+        <img src="banner.jpg">  
+
+        <h2>Profile</h2>
+        <img src="">
+
+        <h2>Biography</h2>
+        // deixar anotado https://night-child.net/psycho-sonic-boom
+
+
   </div>
 </template>
 
@@ -43,4 +62,10 @@ export default {
 </script>
 
 <style>
+
+nav li {
+  display: inline;
+  margin: 0 0 0 15px;
+}
+
 </style>
